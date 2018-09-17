409b8a14974514fa62ca8a507b634d86d9b275d2
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-09-17 22:04:30 +0100

    on loading of page, adds input fields for new task and target date

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,5 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('DOM content loaded');
+  nextPriority = 1;
+  enableNewTask();
 
 })
+
+function enableNewTask() {
+  const table = document.querySelector('.task-table');
+  const newRow = document.createElement('tr');
+  const newPriority = document.createElement('td');
+  const newTask = document.createElement('td');
+  const newTaskInput = document.createElement('input');
+  const newTargetDate = document.createElement('td');
+  const newTargetDateInput = document.createElement('input');
+
+  newPriority.textContent = nextPriority;
+  newPriority.classList.add('priority');
+  newRow.appendChild(newPriority);
+
+  newTask.appendChild(newTaskInput);
+  newRow.appendChild(newTask);
+
+  newTargetDate.appendChild(newTargetDateInput);
+  newRow.appendChild(newTargetDate);
+
+  table.appendChild(newRow);
+
+}
